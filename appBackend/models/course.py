613171b7dd023e914cc7760cfe181fe02c5979ae
themafto <weqqe7@gmail.com>
2025-02-18 17:49:04 +0100

    Authentication --> done

---
+++ appBackend/models/course.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, ARRAY
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+
+from appBackend.db.session import Base
+
+
+class Course(Base):
+    __tablename__ = 'courses'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    lessons_count: Mapped[int] = mapped_column(Integer)
+    lessons_duration: Mapped[int] = mapped_column(Integer)
+    files = Column(ARRAY(String))  # Для PostgreSQL
+    teacher_id = Column(Integer, ForeignKey('our_users.id'))
+
+    users = relationship("OurUsers", back_populates="courses")
