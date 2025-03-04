4c6f5fbf4ef6bb5c2d830b134b37896662293862
themafto <weqqe7@gmail.com>
2025-03-04 19:04:30 +0100

    add router to main file

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from backend.controllers import auth, courses
+from backend.controllers import auth, courses, students
 from backend.database import Base, engine
 from backend.middlewares.cors import setup_cors
 
@@ -11,5 +11,6 @@ setup_cors(app)
 Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(courses.router)
+app.include_router(students.router)
 
 
